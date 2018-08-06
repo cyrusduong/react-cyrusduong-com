95cf68a884e1174bfa05979a60382e9c03c95699
Cyrus Duong <Cyrus.Duong@cerner.com>
2018-08-06 14:18:13 -0500

    Configure HMR for development w/ React.

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,34 @@ module.exports = {
   entry: './src/index.js',
   mode: 'production',
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      options: { presets: ['env'] }
-    }, {
-      test: /\.css$/,
-      use: [ 'style-loader', 'css-loader' ]
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react'],
+          plugins: ['react-hot-loader/babel']
+        }
+      }, {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
   },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: 'dist',
+    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'public/'),
+    hotOnly: true,
     port: 6969,
     publicPath: 'http://localhost:6969/dist/',
-    watchContentBase: true
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 }
