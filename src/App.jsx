50290352ce176c43e86c8aff61b08d6360c2027e
Cyrus Duong <Cyrus.Duong@cerner.com>
2018-08-06 17:37:43 -0500

    Configure prettier and eslint with standardjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello there...
-      </div>
-    )
+  render () {
+    return <div>Hello there...</div>
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
